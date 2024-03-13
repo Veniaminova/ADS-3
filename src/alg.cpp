23dfa6e912777043d7081ac77336f97d03970502
Анастасия <98629650+Veniaminova@users.noreply.github.com>
2024-03-13 17:16:17 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,38 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int count = 0;
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value) {
+            count++;
+            for (int i = mid - 1; i >= 0; i--) {
+                if (arr[i] == value) {
+                    count++;
+                }
+                else {
+                    break;
+                }
+            }
+            for (int i = mid + 1; i < size; i++) {
+                if (arr[i] == value) {
+                    count++;
+                }
+                else {
+                    break;
+                }
+            }
+            break;
+        }
+        else if (arr[mid] < value) {
+            left = mid + 1;
+        }
+        else {
+           right = mid - 1;
+        }
+    }
+    return count;
 }
